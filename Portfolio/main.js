cd7aad2ed1fa7e2b9a5655fea52f3ac9e35ce8f0
hellonirob <119865647+hellonirob@users.noreply.github.com>
2022-12-05 20:18:28 +0600

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector("#menu-btn");
+let header = document.querySelector(".header");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  header.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  header.classList.remove("active");
+};
+
+let themeToggler = document.querySelector("#theme-toggler");
+
